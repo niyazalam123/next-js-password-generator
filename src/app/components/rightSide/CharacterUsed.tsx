c90720ad3e34f32522b34aa24fff00c34512c1d9
Md Niyaz Alam <alamniyaz173@gmail.com>
2024-01-02 15:49:18 +0530

    password filter functionality is working according to checkbox

--- src/app/components/rightSide/CharacterUsed.tsx
+++ src/app/components/rightSide/CharacterUsed.tsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const CharacterUsed = () => {
+interface Props{
+    capital:boolean;
+    small:boolean;
+    digit:boolean;
+    charc:boolean
+}
+
+interface PropsData {
+    setConCgd:React.Dispatch<React.SetStateAction<Props>>;
+    condChd:Props;
+    passwordGenerator:(arg:number)=>void;
+    passwordLength:number;
+}
+const CharacterUsed:React.FC<PropsData> = ({setConCgd,condChd,passwordGenerator,passwordLength}) => {
+    
+    useEffect(()=>{
+        passwordGenerator(passwordLength)
+    },[condChd])
+    function characterSelect(e:any){
+        const {name,checked} = e.target;
+        setConCgd((prev):Props=>{
+            return {
+                ...prev,
+                [name]:checked
+            }
+        })
+    }
     return (
         <>
             <div className='_char1'>
@@ -10,19 +36,43 @@ const CharacterUsed = () => {
                     </div>
                     <div className='_char4'>
                         <div className='_char5'>
-                            <input type="checkbox" name="" id="one1"/>
+                            <input 
+                                type="checkbox" 
+                                name="capital" 
+                                id="one1" 
+                                onChange={characterSelect} 
+                                checked = {condChd.capital}
+                            />
                             <label htmlFor="one1">ABC</label>
                         </div>
                         <div className='_char5'>
-                            <input type="checkbox" name="" id="one2" />
+                            <input 
+                                type="checkbox" 
+                                name="small" 
+                                id="one2" 
+                                onChange={characterSelect} 
+                                checked = {condChd.small}
+                            />
                             <label htmlFor="one2">abc</label>
                         </div>
                         <div className='_char5'>
-                            <input type="checkbox" name="" id="one3" />
+                            <input 
+                                type="checkbox" 
+                                name="digit" 
+                                id="one3" 
+                                onChange={characterSelect} 
+                                checked = {condChd.digit}
+                            />
                             <label htmlFor="one3">123</label>
                         </div>
                         <div className='_char5'>
-                            <input type="checkbox" name="" id="one4" />
+                            <input 
+                                type="checkbox" 
+                                name="charc" 
+                                id="one4" 
+                                onChange={characterSelect} 
+                                checked = {condChd.charc}
+                            />
                             <label htmlFor="one4">#$&</label>
                         </div>
                     </div>
--- src/app/components/rightSide/RightPart.tsx
+++ src/app/components/rightSide/RightPart.tsx
@@ -8,19 +8,37 @@ interface Props {
     passwordLength: number;
     setPasswordLength: React.Dispatch<React.SetStateAction<number>>
 }
+
+interface Props2{
+    capital:boolean;
+    small:boolean;
+    digit:boolean;
+    charc:boolean
+}
+
 const RightPart: React.FC<Props> = ({ passwordLength, setPasswordLength }) => {
     const [password, setPassword] = useState<string>("dn7CEOJW1omadOKcJY12");
     const [capitalAlphbet, SetCapitalAlphbet] = useState<string>("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     const [smallAlphabet, SetSmallAlphabet] = useState<string>("abcdefghijklmnopqrstuvwxyz");
     const [digit, setDigit] = useState<string>("1234567890");
     const [specialCharc, setSpecialCharc] = useState<string>("@#$%^&*)({}~][?><");
+    const [condChd,setConCgd] = useState<Props2>({
+        capital:true,
+        small:false,
+        digit:false,
+        charc:false
+    });
 
     function passwordGenerator(number2: number) {
-        const all_character = capitalAlphbet + smallAlphabet + digit + specialCharc;
+        const all_character = 
+        `
+            ${condChd.capital?capitalAlphbet:""}${condChd.small?smallAlphabet:""}${condChd.digit?digit:""}${condChd.charc?specialCharc:""}
+        `
+        const final_character = all_character.trim();
         let newPassword = "";
         for (let i = 0; i < passwordLength; i++) {
-            const randomIndex = Math.floor(Math.random() * all_character.length);
-            newPassword = newPassword + all_character[randomIndex]
+            const randomIndex = Math.floor(Math.random() * final_character.length);
+            newPassword = newPassword + final_character[randomIndex]
         }
         setPassword(newPassword);
     };
@@ -39,7 +57,12 @@ const RightPart: React.FC<Props> = ({ passwordLength, setPasswordLength }) => {
                         setPasswordLength={setPasswordLength} 
                         passwordGenerator = {passwordGenerator}
                     />
-                    <CharacterUsed />
+                    <CharacterUsed 
+                      condChd={condChd}
+                      setConCgd={setConCgd}
+                      passwordGenerator ={passwordGenerator}
+                      passwordLength={passwordLength} 
+                    />
                 </div>
             </div>
         </>
